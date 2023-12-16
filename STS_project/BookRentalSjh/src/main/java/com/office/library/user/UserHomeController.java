d37276183f6f59864833269980e633da92c1b4c0
신종호 <sjh0804_22@naver.com>
2023-12-16 20:19:44 +0900

    feat: 사용자 회원가입 기능추가

--- STS_project/BookRentalSjh/src/main/java/com/office/library/user/UserHomeController.java
+++ STS_project/BookRentalSjh/src/main/java/com/office/library/user/UserHomeController.java
@@ -1,10 +1,53 @@
 package com.office.library.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/user")
-public class UserHomeController {
+import com.office.library.user.member.UserMemberService;
+import com.office.library.user.member.UserMemberVo;
 
+@Controller
+@RequestMapping("/user/member")
+public class UserHomeController {
+	
+	@Autowired
+	UserMemberService userMemberService;
+	
+	@GetMapping({"","/"})
+	public String home() {
+		System.out.println("[UserHomeController] home()");
+		
+		String nextPage = "user/home";
+		
+		return nextPage;
+	}
+	
+	// 회원가입 
+	@GetMapping("/createAccountForm")
+	public String createAccountForm() {
+		System.out.println("[UserHomeController] createAccountForm()");
+		
+		String nextPage = "user/member/create_account_form";
+		
+		return nextPage;
+	}
+	
+	// 회원가입 확인 
+	@PostMapping("/createAccountConfirm")
+	public String createAccountConfirm(UserMemberVo userMemberVo) {
+		System.out.println("[UserHomeController] createAccountConfirm()");
+		
+		String nextPage = "user/member/create_account_ok";
+		
+		int result = userMemberService.createAccountConfirm(userMemberVo);
+		
+		if (result <= 0 ) {
+			nextPage = "user/member/create_account_ng";
+		}
+		
+		return nextPage;
+	}
 }
