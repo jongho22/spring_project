9d12838979f1884cd9a646a4e0b54ead9bb3f645
신종호 <sjh0804_22@naver.com>
2023-12-20 16:51:10 +0900

    feat: 사용자 검색기능 추가'

--- STS_project/BookRentalSjh/src/main/java/com/office/library/book/user/BookService.java
+++ STS_project/BookRentalSjh/src/main/java/com/office/library/book/user/BookService.java
@@ -1,8 +1,27 @@
 package com.office.library.book.user;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.office.library.book.BookVo;
 
 @Service
 public class BookService {
-
+	
+	@Autowired
+	BookDao bookDao;
+	
+	public List<BookVo> searchBookConfirm(BookVo bookVo){
+		System.out.println("[BookService] searchBookConfirm()");
+		
+		return bookDao.selectBooksBySearch(bookVo);
+	}
+	
+	public BookVo bookDetail(int b_no) {
+		System.out.println("[BookService] bookDeatil()");
+		
+		return bookDao.selectBook(b_no);
+	}
 }
